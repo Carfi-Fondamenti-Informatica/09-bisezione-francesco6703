1a0f73a696e6fcf24e08dbcf98884bc8f0e19894
francesco6703 <127511152+francesco6703@users.noreply.github.com>
2023-05-20 16:04:37 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,46 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
-int main() {
-   return 0;
+int main(){
+	float a, b, fa, fb, x, fx;
+	cout << "inserire estremi\n"; 
+	cin >> a;
+	cin >> b;
+	fa=a*a*cos(a)+1;
+	fb=b*b*cos(b)+1;
+	while(fa*fb>=0)
+	{
+		cin >> a;
+		cin >> b;
+		fa=a*a*cos(a)+1;
+		fb=b*b*cos(b)+1;
+	}
+	if(fa==0||fb==0)
+	{
+		cout << a;
+		cout << fa;
+	}else
+	{
+	x=a;
+	a=(a+b)/2;
+	fa=a*a*cos(a)+1;
+	do{
+	if (fa*fb>0)
+	{
+		a=x;
+		fa=a*a*cos(a)+1;
+		b=(a+b)/2;
+		fb=b*b*cos(b)+1;
+	}
+	else
+	{
+		x=a;
+		a=(a+b)/2;
+		fa=a*a*cos(a)+1;
+	}
+	}while (fa<0.01 || fb<=0.01);
+	cout << a;
+	cout << fa;
+    }
 }
